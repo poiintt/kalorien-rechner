d12a67897f00efc6e5e1a62c9e8aa6fef099a6d9
poiintt <abendmo@gmail.com>
2021-03-01 19:29:13 +0100

    chore: rename shims

---
+++ src/vuex.d.ts
@@ -0,0 +1,37 @@
+import { Store } from "vuex";
+import { State } from "./store";
+import { ComponentCustomProperties } from "vue";
+
+declare module "@vue/runtime-core" {
+  interface Row {
+    food: string;
+    kcal: number;
+  }
+  interface Column {
+    label: string;
+    field: string;
+    isKey?: boolean;
+  }
+  interface State {
+    authenticated: true;
+    testAccount: {
+      username: string;
+      password: string;
+    };
+    table: {
+      columns: Column[];
+      rows: Row[];
+      messages: {
+        pagingInfo: string;
+        pageSizeChangeLabel: string;
+        gotoPageLabel: string;
+        noDataAvailable: string;
+      };
+    };
+  }
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<State>;
+  }
+}
