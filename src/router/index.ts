3fde21491c231f7356ae37f893f0bdd6ca4717c8
poiintt <abendmo@gmail.com>
2021-03-01 19:29:41 +0100

    chore: add prettier config

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,7 +29,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/overview",
     name: "Overview",
     component: Overview
-  },
+  }
 ];
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
